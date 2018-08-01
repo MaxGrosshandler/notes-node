1a1f60f3a74243849526c4ca6d2edecb9f9bc8da
MaxGrosshandler <maxgrosshandler@gmail.com>
2018-08-01 11:27:19 -0500

    more json fiddling

--- playground/json.js
+++ playground/json.js
@@ -1,6 +1,10 @@
-let obj = {
-  name: 'Max'
-}
-let stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+// let obj = {
+//   name: 'Max'
+// }
+// let stringObj = JSON.stringify(obj);
+// // console.log(typeof stringObj);
+// console.log(stringObj);
+
+let personString = '{"name": "Max", "age": 21}'
+let person = JSON.parse(personString);
+console.log(person.name);
