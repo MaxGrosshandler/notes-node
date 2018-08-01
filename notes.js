b44f8bee8c7ac1693766d63882e32593bd16b19d
MaxGrosshandler <maxgrosshandler@gmail.com>
2018-08-01 10:44:17 -0500

    changed add's behavior

--- notes.js
+++ notes.js
@@ -1,7 +1,9 @@
 console.log("Starting notes.js");
 
+let addNote = (title, body) => {
+console.log('Adding note', title, body);
+};
 
-module.exports.addNote = () => {
-console.log('addNote');
-return 'New note';
+module.exports = {
+  addNote
 }
